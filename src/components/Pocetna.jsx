c72d841b99476a2bb0823346c64479dd56050caf
marijakovac20201023 <kovacmarija307@gmail.com>
2022-03-23 11:13:37 +0100

    omoguceno sortiranje proizvoda rastuce i opadajuce

--- src/components/Pocetna.jsx
+++ src/components/Pocetna.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Proizvod from './Proizvod'
 import './Proizvod.css';
 const Pocetna = ({proizvodi}) =>{ 
+  const [sort,setSort] = useState(true);
+  function sortAsc(){
+       setSort(true);
+  }
+  function sortDesc(){
+    setSort(false);
+}
    return (
     <div className='ponuda'>
+        <div className="buttons">
+        <button className="sortbtn btn" onClick={sortAsc}>Sortiraj rastuće</button>
+        <button className="sortbtn btn" onClick={sortDesc}>Sortiraj opadajuće</button>
+      </div>
+
+
+      
+        {sort==true ?  
+            <>
+                    {proizvodi
+                    .sort((a,b) => a.cena <  b.cena ? -1: 1)
+                    .map((p)=>(
+                        <Proizvod proizvod={p} key={p.id}></Proizvod>
+                 ))}
+            </>
+        
+        : 
+        <>
+              {proizvodi
+                .sort((a,b) => a.cena <  b.cena ? 1: -1)
+                .map((p)=>(
+                    <Proizvod proizvod={p} key={p.id}></Proizvod>
+            ))}
+        </>
+        
+        
+        }
 
         {proizvodi.map((p)=>(
                 <Proizvod proizvod={p}></Proizvod>
